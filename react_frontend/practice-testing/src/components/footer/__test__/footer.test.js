e388faa425999a08c0c372e48e2a610d91c1b735
Justin Elmore <jelmore1674@pm.me>
2021-10-23 00:43:05 -0500

    added test to footer

--- react_frontend/practice-testing/src/components/footer/__test__/footer.test.js
+++ react_frontend/practice-testing/src/components/footer/__test__/footer.test.js
@@ -1,13 +1,17 @@
-import React from "react";
-import Footer from "../Footer";
+import React from 'react';
+import Footer from '../Footer';
 
-import { shallow } from "enzyme";
+import { shallow } from 'enzyme';
 
-describe("<Header />", () => {
-  it("Header component loads with 4 links", () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.containsMatchingElement(<h1>This is the footer.</h1>)).toBe(
-      true
-    );
-  });
+describe('<Header />', () => {
+	it('Footer Component', () => {
+		const wrapper = shallow(<Footer />);
+		expect(
+			wrapper.containsMatchingElement(<h1>This is the footer.</h1>)
+		).toBe(true);
+	});
+	it('footer component matches snapshot', () => {
+		const wrapper = shallow(<Footer />);
+		expect(wrapper).toMatchSnapshot();
+	});
 });
